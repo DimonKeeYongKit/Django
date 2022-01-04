6c9015d1ae9a9e848d8f80d37704c8f799bbfc45
DimonKeeYongKit <57079926+DimonKeeYongKit@users.noreply.github.com>
2022-01-04 16:22:47 +0800

    admin site customize

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -20,3 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls'))
 ]
+
+# Configure Admin Titles
+admin.site.site_header = "My Club Adminstration Page"
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome To The Admin Area..."
