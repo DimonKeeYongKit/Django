dc7151165980fb19cdc9b54170f96dcc351eaaea
DimonKeeYongKit <57079926+DimonKeeYongKit@users.noreply.github.com>
2022-01-05 11:39:21 +0800

    logout function

--- webapp/members/views.py
+++ webapp/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-# Create your views here.
+
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -16,3 +16,9 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You were Logout.."))
+    return redirect('home')
