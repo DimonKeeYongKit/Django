30d2b59b6660dfcd3de48052cda20b981d0b6f65
DimonKeeYongKit <57079926+DimonKeeYongKit@users.noreply.github.com>
2022-01-05 13:56:31 +0800

    Extra registration(email, fst & lst name)

--- webapp/members/views.py
+++ webapp/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 def login_user(request):
@@ -27,7 +28,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -37,6 +38,6 @@ def register_user(request):
             messages.success(request,("Registration Successfully!"))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
     
     return render(request, 'register_user.html', {'form':form})
